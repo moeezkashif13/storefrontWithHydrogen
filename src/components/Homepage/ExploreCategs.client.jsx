3247a235f1c0151fe79992a7bdbbf64d384211f7
moeezkashif13 <moeezkashif13@gmail.com>
2022-11-19 00:01:55 +0500

    minnor change done

--- src/components/Homepage/ExploreCategs.client.jsx
+++ src/components/Homepage/ExploreCategs.client.jsx
@@ -24,17 +24,31 @@ destroy:true,
 
 }}>
 
-{[1,2,3,4,5].map((elem,index)=>{
+{[{
+    text:'Men',
+    image:'https://images.pexels.com/photos/13776765/pexels-photo-13776765.jpeg?auto=compress&cs=tinysrgb&w=600',
+},{
+    text:'Women',
+    image:'https://images.pexels.com/photos/4554186/pexels-photo-4554186.jpeg?auto=compress&cs=tinysrgb&w=600',
+},{
+    text:'Men',
+    image:'https://images.pexels.com/photos/10955020/pexels-photo-10955020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+},{
+    text:'Women',
+    image:'https://images.pexels.com/photos/4554172/pexels-photo-4554172.jpeg?auto=compress&cs=tinysrgb&w=600',
+},{
+    text:'Men',
+    image:'https://images.pexels.com/photos/12303984/pexels-photo-12303984.jpeg?auto=compress&cs=tinysrgb&w=600',
+}].map((elem,index)=>{
 
     return  <SplideSlide   > <div className={`h-full  ${index%2==0?'bg-[#eee7de]':'bg-[#eeece8]'} px-8 SMMax:px-3 SMMax:py-3     flex   SMMax:flex-col SMMax:my-5 `}>
 
-    <div className='w-[40%] h-[250px] SMMax:w-full bg-green-500  h-full bg-cover  bg-norepeat ' 
+    <div className='w-[40%] h-[250px] SMMax:w-full   h-full bg-cover  bg-norepeat ' 
     
-    style={{backgroundImage:`url(https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg)`}} 
+    style={{backgroundImage:`url(${elem.image})`}} 
     
     
     >
-
 
 
     </div>
@@ -42,7 +56,7 @@ destroy:true,
 
     <div className='w-[60%] SMMax:w-full  h-full pl-7 SMMax:pl-0 flex flex-col  justify-center'>
 
-<h1 className='text-4xl SMMax:text-2xl SMMax:mt-2 font-semibold'>Shop For Men</h1>
+<h1 className='text-4xl SMMax:text-2xl SMMax:mt-2 font-semibold'>Shop For {elem.text}</h1>
 
 
 <p className='mt-4 mb-6 SMMax:mt-2 SMMax:mb-3 font-semibold SMMax:sm text-[#8d8882]  leading-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida </p>
